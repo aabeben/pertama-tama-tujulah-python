a8f59ab4a140e1937086a58650386236e254e585
Beny Hasanudin <bhasanudin@gmail.com>
2021-03-02 21:23:02 +0700

    Menambahkan modul random untuk menghasilkan menit secara acak
    Menambahkan modul time dan gunakan fungsi sleep untuk jedah beberapa menit

--- odd.py
+++ odd.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from random import randint
+from time import sleep
+
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 31, 33, 35, 37, 39,
         41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
@@ -9,3 +12,6 @@ for i in range(5):
         print("This minute seems a little odd.")
     else:
         print("Not an odd minute.")
+
+    wait_time = randint(1, 60)
+    sleep(wait_time)
